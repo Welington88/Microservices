3a20ac7b7938b57d3f30b076a003b499170cb21d
Welington Marquezini Valverde da Silva <welington_marquezini88@live.com>
2022-06-04 12:24:50 -0300

    api pronta

---
+++ Catalog.API/Data/CatalogContextSeed.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+using Catalog.API.Entities;
+using MongoDB.Driver;
+
+namespace Catalog.API.Data
+{
+    public class CatalogContextSeed
+    {
+        public static void SeedData(IMongoCollection<Product> productCollection)
+        {
+            bool existProduct = productCollection.Find(p => true).Any();
+            if (!existProduct)
+            {
+                productCollection.InsertManyAsync(GetMyProducts());
+            }
+        }
+
+        private static IEnumerable<Product> GetMyProducts()
+        {
+            return new List<Product>()
+            {
+                new Product()
+                {
+                    Id = "602d2149e773f2a399064745",
+                    Name = "IPhone X",
+                    Description = "product-1.png",
+                    Image = "product-1.png",
+                    Price = 950.00M,
+                    Category = "Smart Phone"
+                },
+                new Product()
+                {
+                    Id = "602d2149e773f2a39906f645",
+                    Name = "Sansung 10",
+                    Description = "product-2.png",
+                    Image = "product-2.png",
+                    Price = 250.00M,
+                    Category = "Home Kitchen"
+                },
+            };
+        }
+    }
+}
